59ef2b3d6d7e32c39bbc198aa328f6c4d64b9569
softgenai <admin@softgen.ai>
2026-02-23 01:15:42 +0000

    Edit /package.json

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@/contexts/ThemeContext';
 import { Toaster } from "@/components/ui/toaster"
 import { Navbar } from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata = {
@@ -29,6 +30,20 @@ export default function RootLayout({ children }) {
                         <Toaster />
                     </AuthProvider>
                 </ThemeProvider>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZK53H9N420" />
+                <Script id="google-analytics">
+                    {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZK53H9N420');`}
+                </Script>
+                <Script id="clarity" type="text/javascript">
+                    {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "qg1lwxcmlo");`}
+                </Script>
             </body>
         </html>
     );
