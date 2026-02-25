9547c1751476682836f4cc17ed6c115b146d7ded
softgenai <admin@softgen.ai>
2026-02-25 19:05:51 +0000

    Edit /next.config.js

--- next.config.js
+++ next.config.js
@@ -1,40 +1,39 @@
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
-  serverExternalPackages: ['mongodb', 'cubing'],
-  experimental: {
-    webpackBuildWorker: false,
-  },
-  webpack(config, { dev, isServer }) {
-    if (dev) {
-      config.watchOptions = {
-        poll: 2000,
-        aggregateTimeout: 300,
-        ignored: ['**/node_modules'],
-      };
-    }
-    return config;
-  },
-  onDemandEntries: {
-    maxInactiveAge: 10000,
-    pagesBufferLength: 2,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "X-Frame-Options", value: "ALLOWALL" },
-          { key: "Content-Security-Policy", value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: data:; frame-ancestors 'self' https://*.cubing.net https://*.cloudflare.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.cubing.net; connect-src 'self' https://cdn.cubing.net https://*.vercel.com" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-        ],
-      },
-    ];
-  },
+    output: 'standalone',
+    images: {
+        unoptimized: true,
+    },
+    serverExternalPackages: ['mongodb'],
+    experimental: {
+        webpackBuildWorker: false,
+    },
+    webpack(config, { dev, isServer }) {
+        if (dev) {
+            config.watchOptions = {
+                poll: 2000,
+                aggregateTimeout: 300,
+                ignored: ['**/node_modules'],
+            };
+        }
+        return config;
+    },
+    onDemandEntries: {
+        maxInactiveAge: 10000,
+        pagesBufferLength: 2,
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    { key: "X-Frame-Options", value: "ALLOWALL" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "*" },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
